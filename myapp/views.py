05f052e73028c6a077aa5a8b3abdfaf9cac0c263
voiceit-creator <voiceit.creator@gmail.com>
2020-05-26 18:56:13 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from .forms import *
+from .models import *
+import json
+
+# Create your views here.
+def index(request):
+    context = {
+        'form':ContactForm(),
+        'projects':Project.objects.all().order_by('sort_order'),
+    }
+    return render(request, 'index.html', context)
+
+def contact(request):
+    if request.method == "POST":
+        cf = ContactForm(request.POST)
+
+        # Set default fail message
+        message = 'Failed... Try again later.'
+        if(cf.is_valid()):
+            # Create actual object
+            cm = ContactMessage()
+            cm.name = cf.cleaned_data['name']
+            cm.email = cf.cleaned_data['email']
+            cm.message = cf.cleaned_data['message']
+            cm.save()
+
+            # Success Message
+            message = "Sent!"
+
+        return HttpResponse(message)
